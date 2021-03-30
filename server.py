b3e6d5a59ff42e192e859557ca70d722ccc5ea65
welkenburg <48929649+welkenburg@users.noreply.github.com>
2021-03-30 19:53:53 +0200

    commented

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+#py 3.9
+#author : welkenburg
+
 import socket, sys, asyncio
 
 HOST = '192.168.100.82'
@@ -5,38 +8,44 @@ PORT = 502
 
 class client:
 	def __init__(self, ide, username, addr, connection):
-		self.id = ide
-		self.username = username
-		self.ip = addr
-		self.connection = connection
+		self.id = ide					# int, id of the client
+		self.username = username 		# str, username is sent during the first connection
+		self.ip = addr					# tuple, ip adress and port
+		self.connection = connection 	# connecion socket object to send and receive from the client
 
 class server:
 	def __init__(self, name, ip, port=502, clientMax=10):
-		self.name = name
-		self.ip = ip
-		self.port = port
-		self.clientMax = clientMax
-		self.clients = []
+		self.name = name				# str, server's name
+		self.ip = ip					# str, server's ip adress
+		self.port = port				# int, port you want to listen to
+		self.clientMax = clientMax		# int, max number of users
+		self.clients = []				# list of the users
 		self.start()
 
 	def start(self):
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # new socket
+
+		# try to create a server
 		try:
 			self.socket.bind((self.ip, self.port))
 		except:
 			raise "Le serveur n'a pas pu etre créé"
+
+		# server created
 		while 1:
 			print("### Server Started ###")
+
+			# listen for clients
 			self.socket.listen(self.clientMax)
 
+			# a client is trying to connect !
 			con, addr = self.socket.accept()
 			_, username = con.recv(1024).decode("utf8").split("\6")
 			newClient = client(0,username,addr, con)
 			if len(self.clients) > 0:
 				newClient = client(self.clients[-1].id + 1,username,addr, con)
 
-			# nom= "paul"
-			# f"bonjour {nom}"
+			# client connected, creating a thread
 			print (f"new conncetion {addr[0]}:{addr[1]}")
 			con.send(bytes(f"{newClient.id}", "utf8"))
 			con.send(bytes(f"Bienvenue sur {self.name}. Vous Pouvez a present envoyer des messages !", "utf8"))
@@ -44,20 +53,30 @@ class server:
 			asyncio.run(self.handleConnection(newClient))
 			
 
+	# case to case client handling 
 	async def handleConnection(self, client):
 		running = True
+		# listening for event
 		while running:
 			try:
 				ide, msg = client.connection.recv(1024).decode("utf8").split("\6")
 				print(msg)
+			# client closed the chat
 			except ConnectionResetError:
-				return False
-			if msg == "END":
-				con.send("Au revoir !")
-				con.close()
-				running = False
 				msg = f"{client.username} left the chat..."
-			for user in self.clients:
-				user.connection.send(bytes(f"yo", "utf8"))
+				self.clients.remove(client)
+				running = False
 
-serv = server("LES COCHONOUX DU 31", HOST)
+			# client sent the exit() command
+			if msg == "exit()":
+				msg = f"{client.username} left the chat..."
+				client.connection.send("Au revoir !")
+				client.connection.close()
+				running = False
+				
+			for user in self.clients:
+				print(f"sent '{msg} to {user.username}")
+				user.connection.send(bytes(f"{msg}", "utf8"))
+
+if __name__ == "__main__":
+	serv = server("LES COCHONOUX DU 31", HOST)
