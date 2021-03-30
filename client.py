4a61265b8988a3080f3a8ca9c236f6c8bc1871f7
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-03-30 21:41:18 +0200

    add client

---
+++ client.py
@@ -0,0 +1,65 @@
+import socket, sys, asyncio
+
+class UI:
+    status = {
+        "s": "STATUS",
+        "m": "MESSAGE",
+        "e": "ERROR"
+    }
+
+    def show(self, type, msg):
+        print(f"[{self.status[type]}] {msg}")
+
+class Client:
+    
+    def __init__(self, username, host, port):
+        self.username = username
+        self.HOST = host
+        self.PORT = port
+        self.ENCODING = "utf8"
+        self.ui = UI()
+        self.id = None
+        self.__connect()
+        asyncio.run(self.listenSend())
+        self.__listen()
+    
+    def __connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.socket.connect((self.HOST, self.PORT))
+            self.ui.show("s", f"Connected to {self.HOST}:{self.PORT}")
+        except socket.error:
+            self.ui.show("e", "Connection failed")
+            sys.exit()
+        self.send(self.username)
+        self.id = self.__receive()
+        self.ui.show("m", self.__receive())
+
+    def __receive(self):
+        return self.socket.recv(1024).decode(self.ENCODING)
+
+    def send(self, msg):
+        self.socket.send(bytes(f"{self.id}\6{msg}", self.ENCODING))
+    
+    def close(self):
+        self.socket.close()
+        self.ui.show("s", "Connection closed")
+    
+    async def __listen(self):
+        while True:
+            if self.__receive() == "END":
+                break
+            if self.__receive() != "":
+                self.ui.show("m", self.__receive())
+        self.close()
+    
+    async def listenSend(self):
+        while True:
+            m = input("msg: ")
+            if m == "END":
+                break
+            self.send(m)
+
+HOST = "192.168.100.82"
+PORT = 502
+c = Client("flo", HOST, PORT)
